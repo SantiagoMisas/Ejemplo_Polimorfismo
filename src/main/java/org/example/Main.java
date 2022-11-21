4f1592f250eaca92b82aabc81a77fe185642ebb3
Sasory1 <sasory221@gmail.com>
2022-11-21 11:48:40 -0500

    Se crea repositorio con ejemplo de clase

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,138 @@
+package org.example;
+
+import org.example.clasesHalloween.Fiesta;
+import org.example.clasesHalloween.Invitado;
+import org.example.clasesHalloween.Lugar;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Scanner entradaPorTeclado=new Scanner(System.in);
+        Lugar objetoLugar = new Lugar();
+        Fiesta objetoFiesta = new Fiesta();
+
+        //Creando nuestro primer arraylist
+        ArrayList <Invitado> invitados=new ArrayList<Invitado>();
+
+        System.out.println("****** GRAN FIESTA GRUPO URIBE********");
+        System.out.println("**************************************");
+
+        //CICLO PARA UBICAR INVITADOS DENTRO DEL AFORO
+        int variableDeControl=0;
+        System.out.println("Menu del programa");
+        System.out.println("**************");
+        System.out.println("0. SALIR");
+        System.out.println("1. Registro del Lugar");
+        System.out.println("2. Registro de la fiesta");
+        System.out.println("3. Registro de Invitado");
+        System.out.println("4. Ver Invitados");
+        System.out.println("5. Ver COSTOS");
+        System.out.println("6. Ver GANANCIAS");
+
+
+        do{
+            System.out.println("Digita una opcion del menu: ");
+            variableDeControl=entradaPorTeclado.nextInt();
+
+
+            switch (variableDeControl){
+
+                case 1:
+                    System.out.print("Digita el aforo: ");
+                    objetoLugar.setAforo(entradaPorTeclado.nextInt());
+                    System.out.print("Digita la direccion: ");
+                    objetoLugar.setDireccion(entradaPorTeclado.next());
+                    break;
+                case 2:
+                    System.out.print("Fecha: ");
+                    objetoFiesta.setFecha(entradaPorTeclado.next());
+                    System.out.print("Costos de alimentos: ");
+                    objetoFiesta.setCostosAlimentos(entradaPorTeclado.nextDouble());
+                    System.out.print("Costos de bebidas: ");
+                    objetoFiesta.setCostosBebidas(entradaPorTeclado.nextDouble());
+                    System.out.print("Costos de lugar: ");
+                    objetoFiesta.setCostosLugar(entradaPorTeclado.nextDouble());
+                    System.out.print("Costos de equipos: ");
+                    objetoFiesta.setCostosEquipos(entradaPorTeclado.nextDouble());
+                    break;
+                case 3:
+
+                    if(objetoLugar.getAforo()>=1){
+
+                        //Restamos en 1 el aforo total cada que entra alguien a la fiesta
+                        objetoLugar.setAforo(objetoLugar.getAforo()-1);
+
+                        //si voy a guardar objetos de una clase CUALQUIERA
+                        //en un arraylist, primero debo crear un objeto de
+                        //ESA CLASE CUALQUIERA
+                        Invitado objetoInvitado = new Invitado();
+
+                        //LLENAR/PEDIR ALMACENAR/SETEAR/LLEVAR LOS ATRIBUTOS
+                        //DEL OBJETO QUE ACABO DE CREAR
+                        System.out.println("Digita el nombre del invitado: ");
+                        objetoInvitado.setNombres(entradaPorTeclado.next());
+
+                        //si ya llene mi objeto entonces agregamos
+                        //el objeto a la lista
+                        invitados.add(objetoInvitado);
+
+                    }else{
+
+                        System.out.println("Aragan, no te enra mas gente ahi");
+
+
+                    }
+
+                    break;
+                case 4:
+
+                    //COMO RECORRO UN ARRAYLIST?
+                    //con un ciclo for ADAPTADO recorremos facillmente un ARRAYLIST
+                    for(Invitado invitado:invitados){
+
+                        System.out.println(invitado.getNombres());
+
+                    }
+
+                    break;
+
+                case 5:
+                    System.out.println("El costo de la fiesta fue: "+objetoFiesta.calcularCostosTotales());
+                    break;
+
+                case 6:
+                    //llamar al metodo que calcula ganancias
+                    double ganancias=objetoFiesta.calcularGanancias(invitados.size());
+                    System.out.println("las ganancias fueron: "+ganancias);
+                    break;
+
+                default:
+                    System.out.println("Selecciona una opcion valida");
+
+
+
+            }
+
+
+        }while(variableDeControl != 0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/main/java/org/example/clasesHalloween/Fiesta.java
@@ -0,0 +1,128 @@
+package org.example.clasesHalloween;
+
+public class Fiesta {
+    //ATRIBUTOS
+    private String fecha;
+    private double costosAlimentos;
+    private double costosBebidas;
+    private double costosLugar;
+    private double costosEquipos;
+
+    private final double VALOR_ENTRADA=50000;
+
+    // CONSTRUCTOR
+
+    public Fiesta() {
+    }
+
+    public Fiesta(String fecha, double costosAlimentos, double costosBebidas, double costosLugar, double costosEquipos) {
+        this.fecha = fecha;
+        this.costosAlimentos = costosAlimentos;
+        this.costosBebidas = costosBebidas;
+        this.costosLugar = costosLugar;
+        this.costosEquipos = costosEquipos;
+    }
+
+    //METODOS
+
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public double getCostosAlimentos() {
+        return costosAlimentos;
+    }
+
+    public void setCostosAlimentos(double costosAlimentos) {
+
+        if(costosAlimentos>0){
+            this.costosAlimentos = costosAlimentos;
+        }else{
+            System.out.println("No se aceptan costos negativos");
+        }
+
+    }
+
+    public double getCostosBebidas() {
+        return costosBebidas;
+    }
+
+    public void setCostosBebidas(double costosBebidas) {
+        if(costosBebidas>0){
+            this.costosBebidas = costosBebidas;
+        }else{
+            System.out.println("No se aceptan costos negativos");
+        }
+    }
+
+    public double getCostosLugar() {
+        return costosLugar;
+    }
+
+    public void setCostosLugar(double costosLugar) {
+        if(costosLugar>0){
+            this.costosLugar = costosLugar;
+        }else{
+            System.out.println("No se aceptan costos negativos");
+        }
+    }
+
+    public double getCostosEquipos() {
+        return costosEquipos;
+    }
+
+    public void setCostosEquipos(double costosEquipos) {
+        if(costosEquipos>0){
+            this.costosEquipos = costosEquipos;
+        }else{
+            System.out.println("No se aceptan costos negativos");
+        }
+    }
+
+    public double getVALOR_ENTRADA() {
+        return VALOR_ENTRADA;
+    }
+
+
+    public double calcularCostosTotales(double iva){
+        double costos=this.costosAlimentos+this.costosBebidas+
+                this.costosEquipos+this.costosLugar;
+        //calcular el porcentaje del iva:
+        double valorIVACalculado=costos*iva;
+        //calcular el iva+costoinicial
+        double costosFinales=costos+valorIVACalculado;
+        return costosFinales;
+    }
+
+    public double calcularCostosTotales(){
+        double costos=this.costosAlimentos+this.costosBebidas+
+                this.costosEquipos+this.costosLugar;
+        return costos;
+    }
+
+    public double calcularGanancias(int numeroInvitados){
+
+        double gananciasBrutas=numeroInvitados*this.VALOR_ENTRADA;
+        double gastosTotales=this.calcularCostosTotales();
+        double gananciasNetas=gananciasBrutas-gastosTotales;
+
+        return gananciasNetas;
+
+    }
+
+    public double calcularGanancias(int numeroInvitados, double gastos){
+
+        double gananciasBrutas=numeroInvitados*this.VALOR_ENTRADA;
+        double gananciasNetas=gananciasBrutas-gastos;
+
+        return gananciasNetas;
+
+    }
+
+
+}
